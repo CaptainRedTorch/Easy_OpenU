5f7fcf3683e79ddc159d1a135152bc1f8fb467c8
CaptainRed <CaptainRedtorch@gmail.com>
2025-09-20 15:41:29 +0300

    added toggle switch to course filter function
    added a toggle switch on the webpage of the courses, so that user can choose if to hide or show filtered courses.
    refresh page resets on porpuse.
    
    minor adjustments to info page.
    added contributor list link.

--- scripts/courseFilter.js
+++ scripts/courseFilter.js
@@ -8,16 +8,49 @@ const blockedStatuses = [
 	"נדחה", // Postponed
 ];
 
+let toggle = true
+
+async function filterCourses(toggle) {
 // Go through each row
-rows.forEach((row) => {
-	const statusCell = row.cells[3]; // 4th column contains the status
-	if (!statusCell) return; // Skip if no status cell
+	rows.forEach((row) => {
+		const statusCell = row.cells[3]; // 4th column contains the status
+		if (!statusCell) return; // Skip if no status cell
 
-	const statusText = statusCell.textContent.trim();
+		const statusText = statusCell.textContent.trim();
 
-	// Status is in blocked list → remove row
-	if (blockedStatuses.some((status) => statusText.includes(status))) {
-		row.remove();
-		return;
-	}
-});
+		// Status is in blocked list → remove row
+		if (blockedStatuses.some((status) => statusText.includes(status))) {
+			if (toggle)
+				row.style.display = "none";
+			else
+				row.style.display = "table-row";
+			return;
+		}
+	});
+}
+
+// Insert toggle switch
+const targetElement = document.querySelector("body > table > tbody > tr:nth-child(5) > td > table")
+// toggleSwitch element
+const htmlToggleSwitch = `
+<div class="switch-container">
+        <label class="switch">
+            <input type="checkbox" id="courseFilterSwitch" checked="checked">
+            <span class="slider round"></span>
+        </label>
+        <label style="font-weight: bold; font-size: 14px;">הסתר: החולף, נדחה, בוטל</label>
+    </div>
+`
+
+if (targetElement)
+{
+	targetElement.insertAdjacentHTML("beforebegin", htmlToggleSwitch);
+}
+
+const toggleSwitch = document.getElementById("courseFilterSwitch");
+toggleSwitch.addEventListener('click', (e) => {
+	toggle = !toggle;
+	filterCourses(toggle);
+})
+
+
