1d5077a5f3d2e4a2b370c23b3e0f770facaf9e63
Strike <benxcohen@gmail.com>
2025-09-23 20:45:13 +0300

    Added a settings option to override default materials link

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,19 +1,26 @@
 const studentIDField = document.getElementById('studentID');
 const courseInfoField = document.getElementById('courseInfo');
+const materialsField = document.getElementById('materialsLink');
 const calendarSwitch = document.getElementById('calendarSwitch');
 
 document.addEventListener('DOMContentLoaded', async () => {
 
     // Check if there's a student ID saved in chrome.storage.local
-    const {studentID} = await chrome.storage.local.get('studentID');
+    const { studentID } = await chrome.storage.local.get('studentID');
     if (studentID) {
         studentIDField.value = studentID;  // Set the input field's value if it exists
     }
 
     // Check if there's a course link saved in chrome.storage.local
-    const {courseInfoLink} = await chrome.storage.local.get('courseInfoLink');
+    const { courseInfoLink } = await chrome.storage.local.get('courseInfoLink');
     if (courseInfoLink) {
         courseInfoField.value = courseInfoLink;  // Set the input field's value if it exists
+    }
+
+    // Check if there's a materials link saved in chrome.storage.local
+    const { materialsLink } = await chrome.storage.local.get('materialsLink');
+    if (materialsLink) {
+        materialsField.value = materialsLink;  // Set the input field's value if it exists
     }
 
     // Check if there's a calendar settings preference saved in chrome.storage.local
@@ -55,6 +62,22 @@ async function saveCourseInfo() {
     const link = courseInfoField.value.trim();
     chrome.storage.local.set({ courseInfoLink: link });
 }
+// Event listeners for materials link field, to save on 'Enter' key press or when the field loses focus
+materialsField.addEventListener('keypress', async (e) => {
+    if (e.key === 'Enter') {
+        await saveMaterialsInfo();
+    }
+});
+
+materialsField.addEventListener('blur', async () => {
+    await saveMaterialsInfo();
+});
+
+async function saveMaterialsInfo() {
+    const link = materialsField.value.trim();
+    chrome.storage.local.set({ materialsLink: link });
+}
+
 
 document.getElementById("deleteButton").addEventListener('click', (e) => {
     studentIDField.value = '';
@@ -68,6 +91,6 @@ document.getElementById("deleteButton").addEventListener('click', (e) => {
 });
 
 calendarSwitch.addEventListener('click', (e) => {
-    chrome.storage.local.set({calendarSwitch: e.target.checked});
+    chrome.storage.local.set({ calendarSwitch: e.target.checked });
 });
 
